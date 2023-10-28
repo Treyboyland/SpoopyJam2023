019b976f0fd0e28c7fd41b063ced96328e0411b1
Taylor Grote <grotese@gmail.com>
2023-10-28 18:14:24 -0500

    Add Countdown timer

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour
+{
+	[SerializeField] rho.RuntimeFloat _time;
+	[SerializeField] float _initialTime;
+
+	void Start()
+	{
+		_time.Value = _initialTime;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (_time.Value > 0)
+		{
+			_time.Value = Mathf.Max(_time.Value - Time.deltaTime, 0);
+		}
+    }
+}
