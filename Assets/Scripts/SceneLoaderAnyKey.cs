bf2106a1395c39ff742de169adeffd1ce6b63571
Roosevelt Boyland III <treyboyland@aol.com>
2023-10-28 19:25:24 -0500

    Title screen finished mostly. Prefabs re-added to main scene

---
+++ Assets/Scripts/SceneLoaderAnyKey.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderAnyKey : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+
+    [SerializeField]
+    float delay;
+
+    float elapsed = 0;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (Input.anyKeyDown && !Input.GetButtonDown("Quit") && elapsed >= delay)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
